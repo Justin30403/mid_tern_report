29af8755213d1b60881e81d9981de42ccc975835
Justin <justin30403@gmail.com>
2022-11-22 18:29:56 +0800

    將密碼顯示部分改成暗碼

--- mid_tern_report/Form2.Designer.cs
+++ mid_tern_report/Form2.Designer.cs
@@ -89,8 +89,9 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(219, 179);
+            this.textBox2.Location = new System.Drawing.Point(219, 183);
             this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
             this.textBox2.Size = new System.Drawing.Size(100, 22);
             this.textBox2.TabIndex = 5;
             // 
